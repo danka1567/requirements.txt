bedb73a5e6f6286b4fbb977e964b11a1d53daafd
john banega don <narutoanimeinspirationquote@gmail.com>
2025-10-21 15:47:02 +0600

    Create ani.js

---
+++ ani.js
@@ -0,0 +1,49 @@
+[
+            {
+                "name": "fullmetal-alchemist-brotherhood-1?",
+                "tmdb_id": 25425,
+                "year": 2009,
+                "episodes": "1-64",
+                "episode_offset": 0 // No offset needed
+            },
+            {
+                "name": "hunter-x-hunter-2?",
+                "tmdb_id": 46298,
+                "year": 2011,
+                "episodes": "65-212",
+                "episode_offset": 64 // Episode 1 in TMDB becomes episode 65 in MegaPlay
+            },
+            {
+                "name": "steinsgate-3?",
+                "tmdb_id": 42509,
+                "year": 2011,
+                "episodes": "213-236",
+                "episode_offset": 212 // Episode 1 in TMDB becomes episode 213 in MegaPlay
+            },
+
+             {
+                "name": "naruto-shippuden-355?",
+                "tmdb_id": 31910,
+                "year": 2007,
+                "episodes": "7882-8381",
+                "episode_offset": 7881 // Episode 1 in TMDB becomes episode 213 in MegaPlay
+            },
+
+
+                {
+                "name": "naruto?",
+                "tmdb_id": 46260,
+                "year": 2011,
+                "episodes": "12352-12571",
+                "episode_offset": 12351 // Episode 1 in TMDB becomes episode 213 in MegaPlay
+            },
+
+
+                {
+                "name": "one piece?",
+                "tmdb_id": 37854,
+                "year": 2011,
+                "episodes": "2142-146179",
+                "episode_offset": 2141 // Episode 1 in TMDB becomes episode 213 in MegaPlay
+            }
+        ]
